ee79e685184355e0ad8d1ef4a80c03d421703913
Atanas Dinkov <Dinkov.A@students.softuni.bg>
2023-08-23 00:17:11 +0300

    adding url path for OwnerPhotoView

--- FishingPortal/picture/urls.py
+++ FishingPortal/picture/urls.py
@@ -5,12 +5,13 @@ from django.urls import path
 from FishingPortal.common.views import like_object
 from FishingPortal.picture.views import (
     BusinessPhotoListView, UploadPictureView, PrivatePhotoView, PhotoDetailView,
-    PhotoEditView, PhotoDeleteView, PhotoListView,
+    PhotoEditView, PhotoDeleteView, PhotoListView, OwnerPhotoView,
 )
 
 urlpatterns = [
     path('upload/', UploadPictureView.as_view(), name='upload_photo'),
     path('my-photos/', PrivatePhotoView.as_view(), name='show_my_photos'),
+    path('owner-photos/', OwnerPhotoView.as_view(), name='owner_photos'),
     path('details/<str:slug>/', PhotoDetailView.as_view(), name='photo_details'),
     path('edit/<str:slug>/', PhotoEditView.as_view(), name='edit_photo'),
     path('delete/<str:slug>/', PhotoDeleteView.as_view(), name='delete_photo'),
