a9d6442c50e6cd8bb75b37e8a67998ee386a7277
Atanas Dinkov <Dinkov.A@students.softuni.bg>
2023-08-23 00:13:46 +0300

    optimazing filter classes. adding a base filter class

--- FishingPortal/picture/filters.py
+++ FishingPortal/picture/filters.py
@@ -12,39 +12,42 @@ from FishingPortal.picture.models import Picture
 UserModel = get_user_model()
 
 
-class PictureFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')  # Filter title with case-insensitive partial match
+class BasePictureFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
     upload_date = django_filters.DateFilter(field_name='upload_date', lookup_expr='exact')
-    # Filter by exact upload date
 
+    class Meta:
+        model = Picture
+        fields = ['title', 'upload_date']
+
+
+class PictureFilter(BasePictureFilter):
     related_adventure = django_filters.ModelChoiceFilter(queryset=Adventure.objects.all())
     related_business = django_filters.ModelChoiceFilter(queryset=Business.objects.all())
+    related_competition = django_filters.ModelMultipleChoiceFilter(queryset=Competition.objects.all(),
+                                                                   widget=forms.CheckboxSelectMultiple)
+    uploader = django_filters.ModelChoiceFilter(queryset=UserModel.objects.all())
 
-    # For ManyToManyField relations, I am using ModelMultipleChoiceFilter
+
+class PictureFilterPrivate(BasePictureFilter):
+    related_adventure = django_filters.ModelChoiceFilter(queryset=Adventure.objects.all())
     related_competition = django_filters.ModelMultipleChoiceFilter(queryset=Competition.objects.all(),
                                                                    widget=forms.CheckboxSelectMultiple)
 
-    # For filtering by the user who uploaded the picture
-    uploader = django_filters.ModelChoiceFilter(
-        queryset=UserModel.objects.all())
-
-    class Meta:
-        model = Picture
-        fields = ['title', 'upload_date', 'related_adventure', 'related_business', 'related_competition', 'uploader']
-
-
-class PictureFilterPrivate(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
-        super(PictureFilterPrivate, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        self.filters['related_adventure'].queryset = Adventure.objects.filter(adventurer=user)
+        self.filters['related_competition'].queryset = Competition.objects.filter(participants=user)
 
-        if user:
-            self.title = django_filters.CharFilter(lookup_expr='icontains')
-            self.upload_date = django_filters.DateFilter(field_name='upload_date', lookup_expr='exact')
-            self.filters['related_adventure'].queryset = Adventure.objects.filter(adventurer=user)
-            self.filters['related_competition'].queryset = Competition.objects.filter(participants=user)
 
-    class Meta:
-        model = Picture
-        fields = ['title', 'upload_date', 'related_adventure', 'related_competition']
+class PictureFilterOwner(BasePictureFilter):
+    related_business = django_filters.ModelChoiceFilter(queryset=Business.objects.all())
+    related_competition = django_filters.ModelMultipleChoiceFilter(queryset=Competition.objects.all(),
+                                                                   widget=forms.CheckboxSelectMultiple)
 
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        self.filters['related_business'].queryset = Business.objects.filter(owner=user)
+        self.filters['related_competition'].queryset = Competition.objects.filter(business__owner=user)
